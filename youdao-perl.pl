3c67a2506ddc6f690291248771b8f814c82649db
Guotf <tfguo369@gmail.com>
2013-03-28 14:43:18 +0800

    Init.

---
+++ youdao-perl.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+#########     Author:guotf@Guotft
+#########       Time:2012/11/25 07:56:57
+
+use strict;
+#use warnings;
+use 5.010;
+
+use Encode;
+use LWP::Simple;
+use Data::Dumper;
+use XML::Simple;
+
+binmode(STDIN, ':encoding(utf8)');
+binmode(STDOUT, ':encoding(utf8)');
+binmode(STDERR, ':encoding(utf8)');
+
+my $API_KEY = "1453965843";
+my $KEYFROM = "Shell-YouDao-Dict";
+
+my $DISPLAY_SIZE = 20;
+
+foreach my $word (@ARGV){
+  my $result = get("http://fanyi.youdao.com/openapi.do?keyfrom=" . $KEYFROM . "&key=" . $API_KEY . "&type=data&doctype=xml&version=1.1&q=" . $word);
+
+#say somthing
+#my $result = get("http://www.baidu.com");
+#say $result;
+#say "-------------------------";
+
+
+  # 强制 将子个数为 1 的元素替换为一个数组
+  my $config = XMLin($result, ForceArray => ['ex']);
+
+#  print Dumper($config);
+
+  my $BASIC_EXPLAINS = $config->{basic}->{explains}->{ex};
+
+  my $_size_dis = ($DISPLAY_SIZE - length $word) / 2;
+
+  printf " "x$_size_dis . "\033[7m " . $word . "\033[m" . " "x$_size_dis . "\n";
+  foreach (@{$BASIC_EXPLAINS}) {
+	say $_;
+  }
+
+#  say $config->{basic}->{explains}->{ex}->[1];
+}
